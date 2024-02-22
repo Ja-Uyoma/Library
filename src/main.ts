6c56d30d5d0f7f8a17d5e600fcea0cea1834db6a
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-22 11:57:27 +0300

    feat: add sample data to myLibrary array

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import Book from "./Book";
+
+let myLibrary: Book[] = [
+  new Book("A Game of Thrones", "George R. R. Martin", 694, true),
+  new Book("A Clash of Kings", "George R. R. Martin", 761, true),
+  new Book("A Storm of Swords", "George R. R. Martin", 973, true),
+  new Book("A Feast for Crows", "George R. R. Martin", 753, true),
+  new Book("A Dance with Dragons", "George R. R. Martin", 1016, true),
+];
