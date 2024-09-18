be7f00a3661bad761758bfcb741fa3f438d4046a
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-18 12:08:47 +0300

    test: add test case for title getter

--- src/Book.test.ts
+++ src/Book.test.ts
@@ -13,4 +13,15 @@ describe("Book", () => {
       "A Game of Thrones by George R. R. Martin, 694 pages, read"
     );
   });
+
+  test("method title gets the book title", () => {
+    const agot = new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      694,
+      true
+    );
+
+    expect(agot.title).toBe("A Game of Thrones");
+  });
 });
