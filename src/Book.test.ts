bf99420717df31c463610719444ac11925a10ebb
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-18 11:58:27 +0300

    refactor: move test inside Book test suite

--- src/Book.test.ts
+++ src/Book.test.ts
@@ -1,9 +1,16 @@
 import Book from "./Book";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
-test("default constructed values", () => {
-  const agot = new Book("A Game of Thrones", "George R. R. Martin", 694, true);
-  expect(agot.info()).toBe(
-    "A Game of Thrones by George R. R. Martin, 694 pages, read"
-  );
+describe("Book", () => {
+  test("default constructed values", () => {
+    const agot = new Book(
+      "A Game of Thrones",
+      "George R. R. Martin",
+      694,
+      true
+    );
+    expect(agot.info()).toBe(
+      "A Game of Thrones by George R. R. Martin, 694 pages, read"
+    );
+  });
 });
