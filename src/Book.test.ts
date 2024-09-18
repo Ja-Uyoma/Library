85761378467f05a20e91809375a8495ffe4fdd9c
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-18 11:57:33 +0300

    style: format using Prettier

--- src/Book.test.ts
+++ src/Book.test.ts
@@ -3,5 +3,7 @@ import { expect, test } from "vitest";
 
 test("default constructed values", () => {
   const agot = new Book("A Game of Thrones", "George R. R. Martin", 694, true);
-  expect(agot.info()).toBe("A Game of Thrones by George R. R. Martin, 694 pages, read");
+  expect(agot.info()).toBe(
+    "A Game of Thrones by George R. R. Martin, 694 pages, read"
+  );
 });
